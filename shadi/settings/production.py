403f3fecc6398313b2bb773b2e96631130af819c
rakesh13145sinha <rakeshsinha8292@gmail.com>
2023-02-09 14:00:43 +0530

    update produ file

--- shadi/settings/production.py
+++ shadi/settings/production.py
@@ -1,42 +1,47 @@
 from .base import *
 
 
-DEBUG=config('PRODUCTION_DEBUG')
-print(config('PRODUCTION_DEBUG'))
+# DEBUG=config('PRODUCTION_DEBUG')
+# print(config('PRODUCTION_DEBUG'))
 
 
 
 
 
-print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-print([str(host) for host in config('PRODUCTION_ALLOWED_HOSTS').split(",")])
-print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-ALLOWED_HOSTS=[str(host) for host in config('PRODUCTION_ALLOWED_HOSTS').split(",")]
+# print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+# print([str(host) for host in config('PRODUCTION_ALLOWED_HOSTS').split(",")])
+# print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+# #ALLOWED_HOSTS=[str(host) for host in config('PRODUCTION_ALLOWED_HOSTS').split(",")]
+# ALLOWED_HOSTS=[str(host) for host in config('PRODUCTION_ALLOWED_HOSTS').split(",")]
+
+# print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+# print(config('PRODUCTION_SECRET_KEY'))
+# print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+# SECRET_KEY=config('PRODUCTION_SECRET_KEY')
 
 
-print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-print(config('PRODUCTION_SECRET_KEY'))
-print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-SECRET_KEY=config('PRODUCTION_SECRET_KEY')
+DEBUG=True
 
+ALLOWED_HOSTS=['18.60.217.96']
 
+SECRET_KEY='$j6wor$w23l%_sslus)ro4$4b&a37oww)-#$b9^dg@%%#*s_p!'
 
 
 
 # SERVER Database
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':config('PRO_DB_NAME'),
-        'USER': config('PRO_DB_USER'),
-        'PASSWORD': config('PRO_DB_PASSWORD'),
-        'HOST': config('PRO_DB_HOST'),
-        'PORT': config('PRO_DB_PORT')
-        }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME':config('PRO_DB_NAME'),
+#         'USER': config('PRO_DB_USER'),
+#         'PASSWORD': config('PRO_DB_PASSWORD'),
+#         'HOST': config('PRO_DB_HOST'),
+#         'PORT': config('PRO_DB_PORT')
+#         }
    
            
-}
+# }
 
 STATIC_URL='/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static_dir')
@@ -53,12 +58,12 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media_dir')
 
 
 
-CORS_ORIGIN_ALLOW_ALL = False
+# CORS_ORIGIN_ALLOW_ALL = False
 
-CORS_ALLOWED_ORIGINS=config('WHITE_LIST', cast=lambda v: [s.strip() for s in v.split(',')])
-CORS_ORIGIN_WHITELIST = config('WHITE_LIST', cast=lambda v: [s.strip() for s in v.split(',')])
-CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = config('WHITE_LIST', cast=lambda v: [s.strip() for s in v.split(',')])
-CORS_ALLOW_METHODS = ["DELETE","GET","OPTIONS","PATCH","POST","PUT"]
-CORS_ALLOW_HEADERS = ["accept","accept-encoding","authorization","content-type","dnt","origin",
-    "user-agent","x-csrftoken","x-requested-with","Jwt"]
+# CORS_ALLOWED_ORIGINS=config('WHITE_LIST', cast=lambda v: [s.strip() for s in v.split(',')])
+# CORS_ORIGIN_WHITELIST = config('WHITE_LIST', cast=lambda v: [s.strip() for s in v.split(',')])
+# CORS_ALLOW_CREDENTIALS = True
+# CSRF_TRUSTED_ORIGINS = config('WHITE_LIST', cast=lambda v: [s.strip() for s in v.split(',')])
+# CORS_ALLOW_METHODS = ["DELETE","GET","OPTIONS","PATCH","POST","PUT"]
+# CORS_ALLOW_HEADERS = ["accept","accept-encoding","authorization","content-type","dnt","origin",
+#     "user-agent","x-csrftoken","x-requested-with","Jwt"]
