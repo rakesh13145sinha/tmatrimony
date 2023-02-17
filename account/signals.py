96b1779164a50a63b7723d07ededbabf6b347f0f
rakesh13145sinha <rakeshsinha8292@gmail.com>
2023-02-17 15:04:28 +0530

    update

--- account/signals.py
+++ account/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Person,Partner_Preferences,SaveOTP
+
+from account.send_otp import sending_otp
+from .models import Person,Partner_Preferences
 
 from account.commanfunc import get_age
 
@@ -40,11 +42,9 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         instance.matrimony_id=generate_matrimonyid()
         instance.dateofbirth=get_age(instance.dateofbirth)
-        
         instance.save()
-        print(instance.dateofbirth)
-        # sending_otp(random.randint(1000,9999), instance.phone_number)
-        SaveOTP.objects.update_or_create(phone_number=instance.phone_number,otp=1234)
+        sending_otp("2023", instance.phone_number)
+        
         create_partner_preferance(instance.id)
         
 
